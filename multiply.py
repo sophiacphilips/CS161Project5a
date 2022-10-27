1b3771a243c2661eb9ea939680aa699ba3b3a20e
sophiacphilips <114261752+sophiacphilips@users.noreply.github.com>
2022-10-26 19:59:20 -0700

    commit 3

---
+++ multiply.py
@@ -0,0 +1,14 @@
+#Author: Sophia Philips
+#Github Username: sophiacphilips
+#Date:10/25/22
+#This code take two integers and multiplies them using addition via recursion rather than directly multiplying them together.
+
+def multiply(a,b):
+#account for input of 0
+    if a==0:
+        return 0
+    return multiply(a-1,b) + b
+#test
+a=int(input())
+b=int(input())
+print(multiply(a,b))
